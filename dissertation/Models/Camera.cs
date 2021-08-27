d472fb3e37c160d8157640f135d764c43db4f5a8
PavelBelka <58385396+PavelBelka@users.noreply.github.com>
2021-08-27 23:28:46 +0300

    Добавьте файлы проекта.

---
+++ dissertation/Models/Camera.cs
@@ -0,0 +1,35 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dissertation.Models
+{
+    class Camera
+    {
+        private VideoCapture Capture;
+        public double Focus { get; set; }
+        public double Distance { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public Camera() { }
+
+        public Camera(double focus, double distance, double width, double height)
+        {
+            Capture = new VideoCapture();
+            Focus = focus;
+            Distance = distance;
+            Width = width;
+            Height = height;
+        }
+
+        public Mat GetFrame()
+        {
+            Mat image = Capture.QueryFrame();
+            return image;
+        }
+    }
+}
