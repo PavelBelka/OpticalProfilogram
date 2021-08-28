ff8d35fe7c415056cb7a7e2d0b413607cbfab8d2
PavelBelka <58385396+PavelBelka@users.noreply.github.com>
2021-08-28 22:11:35 +0300

    Добавлено Command

---
+++ dissertation/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using dissertation.Commands;
+using System.Windows;
+using System.Windows.Input;
+
+namespace dissertation.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Text_log;
+
+        /// <summary>Текст лога</summary>
+        public string Text_log
+        {
+            set => Set(ref _Text_log, value);
+        }
+
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+ 
+
+        private void OnCloseApplicationCommandExecute(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+        public MainWindowViewModel()
+        {
+            CloseApplicationCommand = new ActionCommand(OnCloseApplicationCommandExecute, CanCloseApplicationCommandExecute);
+        }
+    }
+}
