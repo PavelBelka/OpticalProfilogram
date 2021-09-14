08923c5c360d49736ce1ee2b12587b106c1b10da
PavelBelka <58385396+PavelBelka@users.noreply.github.com>
2021-09-14 20:23:31 +0300

    Выведено изображение на Image

--- dissertation/App.xaml.cs
+++ dissertation/App.xaml.cs
@@ -1,8 +1,18 @@
 ﻿using System.Windows;
+using dissertation.ViewModels;
+using dissertation.Models;
 
 namespace dissertation
 {
     public partial class App : Application
     {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            MainWindow view = new MainWindow();
+            Camera camera = new Camera();
+            MainWindowViewModel viewModel = new MainWindowViewModel(camera);
+            view.DataContext = viewModel;
+            view.Show();
+        }
     }
 }
--- dissertation/ViewModels/MainWindowViewModel.cs
+++ dissertation/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,26 @@
-﻿using dissertation.Commands;
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using dissertation.Commands;
+using dissertation.Models;
+using Emgu.CV;
 
 namespace dissertation.ViewModels
 {
     internal class MainWindowViewModel : BaseViewModel
     {
+        [DllImport("gdi32")]
+        private static extern int DeleteObject(IntPtr o);
+
+        private Camera _camera;
+
         private string _Text_log;
+        private ImageSource _Image_Box;
 
         /// <summary>Текст лога</summary>
         public string Text_log
@@ -15,16 +29,42 @@ namespace dissertation.ViewModels
             set => Set(ref _Text_log, value);
         }
 
+        public ImageSource Image_Box
+        {
+            get => _Image_Box;
+            set => Set(ref _Image_Box, value);
+        }
+
         public ICommand CloseApplicationCommand { get; }
+
+        public ICommand GetFrameCommand { get; }
 
         private bool CanCloseApplicationCommandExecute(object p) => true;
  
 
         private void OnCloseApplicationCommandExecute(object p) => Application.Current.Shutdown();
 
-        public MainWindowViewModel()
+        private bool CanGetFrameCommandExecute(object p) => true;
+
+        private void OnGetFrameCommandExecute(object p)
         {
+            Bitmap img = _camera.GetFrame().ToBitmap();
+            IntPtr ptr = img.GetHbitmap();
+
+            BitmapSource bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                ptr,
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+            DeleteObject(ptr);
+            Image_Box = bs;
+        }
+
+        public MainWindowViewModel(Camera cam)
+        {
+            _camera = cam;
             CloseApplicationCommand = new ActionCommand(OnCloseApplicationCommandExecute, CanCloseApplicationCommandExecute);
+            GetFrameCommand = new ActionCommand(OnGetFrameCommandExecute, CanGetFrameCommandExecute);
             Text_log = "Тест лога";
         }
     }
